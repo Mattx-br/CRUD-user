40ebddbd99d91fb203f2f195a9cf92fb4039fc94
Anthony Nunes <30761865+Mattx-br@users.noreply.github.com>
2022-02-04 15:19:44 -0300

    User and Admin counters

---
+++ controllers/UserController.js
@@ -0,0 +1,162 @@
+class UserController {
+
+    constructor(formId, tableId) {
+        this.formEl = document.querySelector('#' + formId);
+        this.tableEl = document.getElementById(tableId);
+
+        this.onSubmit();
+    }
+
+    onSubmit() {
+
+        this.formEl.addEventListener("submit", e => {
+
+            e.preventDefault();
+
+            let btn = this.formEl.querySelector('[type=submit]');
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            if(!values) { return false; }
+
+            this.getPhoto().then(
+                content => {
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+                },
+                e => {
+                    console.error(e);
+                }
+            );
+
+        });
+
+    }
+
+    // this method gets the photo url and reads the photo file
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo') {
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = e => {
+
+                reject(e);
+
+            };
+            if (file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
+        });
+
+    }
+
+    getValues() {
+
+        let user = {};
+        var isValid = true;
+
+        [...this.formEl.elements].forEach(function(field, index) {
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                field.parentElement.classList.add('has-error');
+
+                isValid = false;
+            }
+
+            if (field.name == 'gender') {
+
+                if (field.checked) { user[field.name] = field.value; }
+
+            } else if (field.name == 'admin') {
+
+                user[field.name] = field.checked;
+
+            } else { user[field.name] = field.value; }
+
+        });
+
+        if(!isValid){
+            return false;
+        }
+
+        return new User(
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin
+        );
+
+    }
+
+    addLine(dataUser) {
+
+        let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `    
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        `;
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    updateCount(){
+
+        let numberUser = 0;
+        let numberAdmin = 0;
+        [...this.tableEl.children].forEach(tr => {
+            
+            numberUser++;
+
+            let user = JSON.parse(tr.dataset.user)
+
+            if (user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector('#number-users').innerHTML = numberUser;
+        document.querySelector('#number-users-admin').innerHTML = numberAdmin;
+    }
+}
